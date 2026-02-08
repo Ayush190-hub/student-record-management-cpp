e2eaadb88bc86f224298202fa133e3fd2e75b9e1
Ayush190-hub <ayusharyan162@gmail.com>
2026-02-08 19:55:40 +0530

    Add initial C++ project structure and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Student Record Management System" << endl;
+    cout << "Project setup successful!" << endl;
+    return 0;
+}
