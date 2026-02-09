b691adaa27818066729c86323cc751b52dcaf3e5
Ayush190-hub <ayusharyan162@gmail.com>
2026-02-09 10:20:54 +0530

    Add Student structure and basic menu skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
+struct Student {
+    int roll;
+    string name;
+    int age;
+    float marks;
+};
+
+vector<Student> students;
+
 int main() {
+    int choice;
+
     cout << "Student Record Management System" << endl;
-    cout << "Project setup successful!" << endl;
+    cout<<"1. Add Student"<<endl;
+    cout<<"2. Display Students"<<endl;
+    cout<<"3. Search Student"<<endl;
+    cout<<"4. Exit"<<endl;
+
+    cout<<"Enter your choice: "<<endl;
+    cin>> choice;
+
+    cout<< "Project setup successful!" << endl;
+ 
     return 0;
 }
